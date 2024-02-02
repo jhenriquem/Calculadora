6d6fc915b7f575ea642fc343626566da241aa789
jhenriquem <machadojh11@gmail.com>
2024-02-02 20:49:16 -0300

    update

--- calc.js
+++ calc.js
@@ -1,24 +1,21 @@
 let equal_pressed = 0;
 
-let output = document.getElementById('output');
-let all_Operations = '+-*/.**';
+let output = document.getElementById("output");
+let all_Operations = "+-*/.**";
 
-let buttons = document.querySelectorAll('.buttons');
+let buttons = document.querySelectorAll(".buttons");
 
-let clean = document.getElementById('btn_clean');
-let result = document.getElementById('btn_result');
-let backspace = document.getElementById('btn_backspace');
-
-window.onload = () => {
-  output.innerHTML = '0';
-};
+let clean = document.getElementById("btn_clean");
+let result = document.getElementById("btn_result");
+let backspace = document.getElementById("btn_backspace");
 
 buttons.forEach((buttons) => {
-  buttons.addEventListener('click', () => {
-    if (output.innerHTML == 'Erro') {
-      output.innerHTML = '';
+  buttons.addEventListener("click", () => {
+    if (output.innerHTML == "Erro") {
+      output.innerHTML = "";
     }
     check_RepeatOperations(buttons.value);
+    output.scrollLeft = output.scrollWidth;
   });
 });
 
@@ -31,20 +28,17 @@ function check_RepeatOperations(current_btnValue) {
       output.innerHTML += current_btnValue;
     }
   } else {
-    if (output.innerHTML.length >= 10) {
-    } else {
-      output.innerHTML += current_btnValue;
-    }
+    output.innerHTML += current_btnValue;
   }
 }
 
 // ---------------- Equal action ----------------------
-result.addEventListener('click', equal);
+result.addEventListener("click", equal);
 
 function equal() {
   equal_pressed = 1;
   let expression = output.innerHTML;
-  document.querySelector('.previous').innerHTML = expression;
+  document.querySelector(".previous").innerHTML = expression;
   try {
     let solution = eval(expression);
     if (Number.isInteger(solution)) {
@@ -53,17 +47,17 @@ function equal() {
       output.innerHTML = `${solution.toFixed(2)}`;
     }
   } catch (err) {
-    output.innerHTML = 'Erro';
+    output.innerHTML = "Erro";
   }
 }
 
 // ---------------- Clean Action ----------------------
-clean.addEventListener('click', () => {
-  output.innerHTML = ' ';
-  document.querySelector('.previous').innerHTML = ' ';
+clean.addEventListener("click", () => {
+  output.innerHTML = " ";
+  document.querySelector(".previous").innerHTML = " ";
 });
 
 // ----------------  Backspace Action -----------------
-backspace.addEventListener('click', () => {
+backspace.addEventListener("click", () => {
   output.innerHTML = output.innerHTML.substring(0, output.innerHTML.length - 1);
 });
